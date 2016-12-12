5e5a025b83e182071a37b65f51efdff793595ca6
jiayanlong <jiayanlong@guazi.com>
2016-12-12 15:26:13 +0800

    fix import proxy

--- download/download.go
+++ download/download.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"time"
 
-	hproxy "github.com/mmczoo/caresm/src/phoneserver/proxy"
+	hproxy "github.com/mmczoo/gotools/proxy"
 
 	"golang.org/x/net/proxy"
 	"golang.org/x/net/publicsuffix"
